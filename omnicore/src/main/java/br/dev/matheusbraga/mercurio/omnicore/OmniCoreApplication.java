53bb4babdf953ae5d64ed91e64a8b4801fea9f08
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-04-09 08:13:33 -0300

    feat(OmniCore): Add OmniCore Java Files

---
+++ omnicore/src/main/java/br/dev/matheusbraga/mercurio/omnicore/OmniCoreApplication.java
@@ -0,0 +1,13 @@
+package br.dev.matheusbraga.mercurio.omnicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniCoreApplication.class, args);
+	}
+
+}
---
+++ omnicore/src/test/java/br/dev/matheusbraga/mercurio/omnicore/OmniCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.matheusbraga.mercurio.omnicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
