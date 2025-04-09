94dc519f7cd0a07c0a109341c22c1f2cedfdf5c1
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-04-09 08:12:55 -0300

    feat(ACL): Add ACL Java files

---
+++ acl/src/test/java/br/dev/matheusbraga/mercurio/acl/AclApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.matheusbraga.mercurio.acl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
