843922e4e7667490e35761c21bd7134d3a6eb500
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2025-04-09 08:13:12 -0300

    feat(Client): Add Client Java files

---
+++ client/src/test/java/br/dev/matheusbraga/mercurio/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.matheusbraga.mercurio.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
